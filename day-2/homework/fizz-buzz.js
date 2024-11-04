d228362a71747a8a64407c632ae98e5465b9cf8b
scruz777 <sarah.cruz0522@gmail.com>
2024-11-03 19:57:21 -0700

    Update fizz-buzz.js

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -12,10 +12,18 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
-
-    // YOUR CODE GOES ABOVE HERE //
+     for (let i = 1; i <= 100; i++) { //use for loop to iterate from 1 to 100 
+        if (i % 3 === 0 && i % 5 === 0) { //use remainder operator to determine if number is multiple of 3 and/or 5 if both multiple of three and five print "fizzbuzz'
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) { //for multiples of 3 print "fizz"
+            console.log("Fizz");
+        } else if (i % 5 === 0) { //for multiples of 5 print "buzz"
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+ // YOUR CODE GOES ABOVE HERE //
 }
 
 
